b48b041be56cf3a48b94f889f4549def6395561e
Justyna Malinowska <justynamalinowska758@gmail.com>
2024-03-03 16:25:16 +0100

    Finish `normal version`

--- Lab01/Script.js
+++ Lab01/Script.js
@@ -2,12 +2,12 @@ const przeliczbtn = document.querySelector("#przelicz");
 przeliczbtn.addEventListener("click", licz);
 const dodajpolebtn = document.querySelector("#dodajPole");
 dodajpolebtn.addEventListener("click", dodajPole);
-// val1.addEventListener("input", licz);
-// val2.addEventListener("input", licz);
-// val3.addEventListener("input", licz);
+val1.addEventListener("input", licz);
+val2.addEventListener("input", licz);
+val3.addEventListener("input", licz);
 let numbers = document.querySelector("#numbers");
 
-let i = 3;
+let counter = 3;
 
 function licz() {
   let sum = 0;
@@ -15,21 +15,20 @@ function licz() {
   let min = Infinity;
   let max = -Infinity;
 
-  for (let j = 1; j <= (numbers.childElementCount - 1) / 2; j++) {
-    const input = document.querySelector("#val" + j);
-    // console.log(i);
-    if (input.value == "" || isNaN(input.value)) {
+  let childNumber = numbers.childElementCount;
+
+  for (let i = 1; i <= childNumber; i++) {
+    const input = document.querySelector("#val" + i);
+    if (input.value == "" && i > 3) {
       numbers.removeChild(input);
-      numbers.removeChild(document.querySelector("#l" + j));
-      i = i - 1;
+      counter--;
     } else {
       const inputVal = parseFloat(input.value);
-      input.addEventListener("input", licz);
       sum += inputVal;
-      avg = sum / i;
       if (inputVal < min) min = inputVal;
       if (inputVal > max) max = inputVal;
     }
+    avg = sum / counter;
   }
 
   document.querySelector("#sum").value = sum;
@@ -39,19 +38,12 @@ function licz() {
 }
 
 function dodajPole() {
-  i = i + 1;
+  counter++;
   let input = document.createElement("input");
-  input.id = "val" + i;
+  input.id = "val" + counter;
   input.type = "number";
+  input.placeholder = "Liczba " + counter;
 
-  let label = document.createElement("label");
-  label.innerHTML = "Liczba " + i + ":";
-  label.id = "l" + i;
-
-  numbers.insertBefore(label, numbers.lastElementChild);
-  numbers.insertBefore(input, numbers.lastElementChild);
-
-  //efElem.insertAdjacentElement(position, newElem)
-  // numbers.insertAdjacentElement("beforeend", label);
-  // numbers.insertAdjacentElement("beforeend", input);
+  numbers.appendChild(input);
+  input.addEventListener("input", licz);
 }
