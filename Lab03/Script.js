c13e9f3ce5263f1748d9aeee9dbf812a6c47b395
Justyna Malinowska <justynamalinowska758@gmail.com>
2024-03-20 23:00:46 +0100

    Start lab03

--- Lab03/Script.js
+++ Lab03/Script.js
@@ -1,9 +1,36 @@
-const clap = document.querySelector("#s1");
-const kick = document.querySelector("#s2");
+const boom = document.querySelector("#s1");
+const clap = document.querySelector("#s2");
+const hihat = document.querySelector("#s3");
+const kick = document.querySelector("#s4");
+const openhat = document.querySelector("#s5");
+const ride = document.querySelector("#s6");
+const snare = document.querySelector("#s7");
+const tink = document.querySelector("#s8");
+const tom = document.querySelector("#s9");
+
+let stopClicked = false;
+
+for (let i = 1; i < 5; i++) {
+  document.querySelector("#start" + i).addEventListener("click", function () {
+    Save(i);
+  });
+}
+
+const recording1 = [];
+const recording2 = [];
+const recording3 = [];
+const recording4 = [];
 
 const sounds = {
   a: document.querySelector("#s1"),
   s: document.querySelector("#s2"),
+  w: document.querySelector("#s3"),
+  d: document.querySelector("#s4"),
+  q: document.querySelector("#s5"),
+  e: document.querySelector("#s6"),
+  z: document.querySelector("#s7"),
+  x: document.querySelector("#s8"),
+  c: document.querySelector("#s9"),
 };
 
 document.addEventListener("keypress", (ev) => {
@@ -23,3 +50,29 @@ document.addEventListener("keypress", (ev) => {
   sound.currentTime = 0;
   sound.play();
 });
+
+function Save(i) {
+  switch (i) {
+    case 1:
+      document
+        .querySelector("#stop" + i)
+        .addEventListener("click", function () {
+          stopClicked = true;
+        });
+      while (!stopClicked) {
+        recording1.push();
+      }
+      break;
+    case 2:
+      break;
+
+    case 3:
+      break;
+
+    case 4:
+      break;
+
+    default:
+      break;
+  }
+}
